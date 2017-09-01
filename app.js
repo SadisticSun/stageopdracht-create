a2168d5df0b137c169aeba467b4b9a67ebc19b71
Tristan Jacobs <tristan@iamt.nl>
2017-09-01 13:33:18 +0200

    changed server settings

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports = app;
